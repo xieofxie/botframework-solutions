099ca8065c8d9db6b1c05c5097bc921bb5f6d183
Hualiang Xie <hualxie@microsoft.com>
2020-02-21 15:55:25 +0800

    add cards for testing

--- samples/csharp/skill/SkillSample/Dialogs/SampleDialog.cs
+++ samples/csharp/skill/SkillSample/Dialogs/SampleDialog.cs
@@ -6,6 +6,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Schema;
 
 namespace SkillSample.Dialogs
 {
@@ -28,6 +30,7 @@ namespace SkillSample.Dialogs
 
             AddDialog(new WaterfallDialog(nameof(SampleDialog), sample));
             AddDialog(new TextPrompt(DialogIds.NamePrompt));
+            AddDialog(new NumberPrompt<int>(DialogIds.TestPrompt));
 
             InitialDialogId = nameof(SampleDialog);
         }
@@ -36,27 +39,69 @@ namespace SkillSample.Dialogs
         {
             // NOTE: Uncomment the following lines to access LUIS result for this turn.
             // var luisResult = stepContext.Context.TurnState.Get<LuisResult>(StateProperties.SkillLuisResult);
-            var prompt = TemplateEngine.GenerateActivityForLocale("NamePrompt");
-            return await stepContext.PromptAsync(DialogIds.NamePrompt, new PromptOptions { Prompt = prompt });
+            var prompt = TemplateEngine.GenerateActivityForLocale("TestPrompt");
+            return await stepContext.PromptAsync(DialogIds.TestPrompt, new PromptOptions { Prompt = prompt });
         }
 
         private async Task<DialogTurnResult> GreetUser(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            dynamic data = new { Name = stepContext.Result.ToString() };
-            var response = TemplateEngine.GenerateActivityForLocale("HaveNameMessage", data);
-            await stepContext.Context.SendActivityAsync(response);
+            int index = (int)stepContext.Result;
+            var text = "Do you want to see tomorrow's schedule?";
+            Activity prompt = null;
+            if (index == 1)
+            {
+                prompt = TemplateEngine.GenerateActivityForLocale("MessageWithCard", CreateCardInput(stepContext.Context, "SelectAction"));
+            }
+            else if (index == 2)
+            {
+                prompt = TemplateEngine.GenerateActivityForLocale("SuggestAction");
+            }
+            else if (index == 3)
+            {
+                prompt = TemplateEngine.GenerateActivityForLocale("HeroCardForSuggestAction");
+            }
+            else if (index == 4)
+            {
+                prompt = TemplateEngine.GenerateActivityForLocale("MessageWithCard", CreateCardInput(stepContext.Context, "Action", text));
+            }
+            else if (index == 5)
+            {
+                prompt = TemplateEngine.GenerateActivityForLocale("MessageWithCard", CreateCardInput(stepContext.Context, "ActionSet", text));
+            }
+            else if (index == 6)
+            {
+                prompt = TemplateEngine.GenerateActivityForLocale("MessageWithCard", CreateCardInput(stepContext.Context, "ToggleChoiceSet"));
+            }
+            else if (index == 7)
+            {
+                prompt = TemplateEngine.GenerateActivityForLocale("MessageWithCard", CreateCardInput(stepContext.Context, "ToggleVisibility"));
+            }
 
-            return await stepContext.NextAsync();
+            await stepContext.Context.SendActivityAsync(prompt);
+            return new DialogTurnResult(DialogTurnStatus.Waiting);
         }
 
         private Task<DialogTurnResult> End(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            return stepContext.EndDialogAsync();
+            return stepContext.ReplaceDialogAsync(nameof(SampleDialog));
+        }
+
+        private object CreateCardInput(ITurnContext context, string name, string text = "")
+        {
+            if (context.Activity.ChannelId == Channels.Msteams)
+            {
+                return new { Text = text, Card = name + ".Teams.json" };
+            }
+            else
+            {
+                return new { Text = text, Card = name + ".json" };
+            }
         }
 
         private class DialogIds
         {
             public const string NamePrompt = "namePrompt";
+            public const string TestPrompt = "testPrompt";
         }
     }
 }
